20663684a015160e1f7362c6b812d3aa72646588
Melik fırat Başçi <mbasci23@hotmail.com>
2021-03-20 19:06:55 +0300

    JWT3

---
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,50 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Security.Encryption;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class JwtHelper : ITokenHelper
+    {
+        public IConfiguration Configuration { get; }
+        TokenOptions _tokenOptions;
+        DateTime _accesTokenExpiration;
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+        }
+        public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
+        {
+            _accesTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccesTokenExpiration);
+            var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+            var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
+
+        }
+        public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, User user,
+            SigningCredentials signingCredentials, List<OperationClaim> operationClaims)
+        {
+            var jwt = new JwtSecurityToken(
+                issuer: tokenOptions.Issuer,
+                audience: tokenOptions.Audience,
+                expires: _accesTokenExpiration,
+                notBefore:DateTime.Now,
+                claims: operationClaims,
+                signingCredentials:signingCredentials
+                
+                );
+        }
+        private IEnumerable<Claim> SetClaims(User user,List<OperationClaim> operationClaims)
+        {
+            var claims = new List<Claim>();
+            claims
+        }
+    }
+}
